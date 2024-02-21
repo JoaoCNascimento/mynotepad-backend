0d730724e22e1269b5b722b2e04d9c9c085b5732
João Nascimento <joao.nascimento0576@gmail.com>
2024-02-21 15:12:01 -0300

    Adjusting mapper profile

--- Core/MyNotepad.Application/Mappings/EntityToDTOMappingProfile.cs
+++ Core/MyNotepad.Application/Mappings/EntityToDTOMappingProfile.cs
@@ -8,7 +8,8 @@ public class EntityToDTOMappingProfile : Profile
 {
     public EntityToDTOMappingProfile()
     {
-        CreateMap<Note, NoteDTO>().ReverseMap();
+        CreateMap<Note, NoteDTO>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
+        CreateMap<NoteDTO, Note>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => new ObjectId(src.Id)));
         CreateMap<User, UserDTO>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
         CreateMap<UserDTO, User>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => new ObjectId(src.Id)));
     }
