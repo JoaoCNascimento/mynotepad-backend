b45a631c56fadfdd53e1b10cb82004ae26700a0b
João Nascimento <joao.nascimento0576@gmail.com>
2024-02-21 01:08:58 -0300

    Fixing models bsonElement annotation

--- Core/MyNotepad.Domain/Entities/BaseEntity.cs
+++ Core/MyNotepad.Domain/Entities/BaseEntity.cs
@@ -8,9 +8,14 @@ public abstract class BaseEntity
     [BsonId]
     public ObjectId Id { get; protected set; }
 
+    [BsonElement("createdAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
     
+    [BsonElement("updatedAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+
+    [BsonElement("__v")]
+    public int Version { get; set; }
 }
--- Core/MyNotepad.Domain/Entities/Note.cs
+++ Core/MyNotepad.Domain/Entities/Note.cs
@@ -7,14 +7,17 @@ using MyNotepad.Domain.Enums;
 namespace MyNotepad.Domain.Entities;
 public class Note : BaseEntity
 {
+    [BsonElement("title")]
     [Required(AllowEmptyStrings = false, ErrorMessage = "TitleRequiredErrorMessage")]
     [MaxLength(50, ErrorMessage = "TitleMaxLengthErrorMessage")]
     public string Title { get; set; } = string.Empty;
 
+    [BsonElement("content")]
     [Required(AllowEmptyStrings = true, ErrorMessage = "ContentRequiredErrorMessage")]
     [MaxLength(300000, ErrorMessage = "ContentMaxLengthErrorMessage")]
     public string Content { get; set; } = string.Empty;
     
+    [BsonElement("color")]
     [BsonRepresentation(BsonType.String)]
     public Colors Color { get; set; }
 }
