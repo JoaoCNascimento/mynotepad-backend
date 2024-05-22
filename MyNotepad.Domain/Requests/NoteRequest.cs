e62fe18802dcc15ecea30a4b6483db733acb9b24
João Nascimento <joao.nascimento0576@gmail.com>
2024-05-22 17:22:04 -0300

    Creating request objects

---
+++ MyNotepad.Domain/Requests/NoteRequest.cs
@@ -0,0 +1,21 @@
+﻿using MyNotepad.Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyNotepad.Domain.Requests
+{
+    public class NoteRequest
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "The 'title' field is required")]
+        [Length(1, 120, ErrorMessage = "The length of 'title' field must be from 1 to 120 characters")]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(2500, ErrorMessage = "The max length of 'description' must be of 2500 characters")]
+        public string? Description { get; set; } = string.Empty;
+        
+        [Required, DefaultValue("blue")]
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ MyNotepad.Domain/Requests/UserRegisterRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNotepad.Domain.Requests
+{
+    public class UserRegisterRequest
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+        
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        
+        [MinLength(6)]
+        [MaxLength(255)]
+        public string Password { get; set; } = string.Empty;
+
+        [MinLength(6)]
+        [MaxLength(255)]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; set; } = string.Empty;
+        
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
